cb414e0637f18aa100aa97ed43607edec18fdea9
picottijunior <picotti.junior@outlook.com>
2015-08-26 17:40:34 -0300

    Update baby 26.08.2015-17.36

--- baby.hs
+++ baby.hs
@@ -26,6 +26,7 @@ rightTri a = [ (a,b,c) | c <- [1..10], b <- [1..c], a^2 + b^2 == c^2]
 rightTri' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
 
 
+
 triangles = [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
 rightTriangles a = [ (a,b,c) | c <- [1..10], b <- [1..c], a^2 + b^2 == c^2]
 rightTriangles' = [ (a,b,c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
@@ -91,3 +92,91 @@ length' (_:xs) = 1 + length' xs  -- dividindo lista entre 1º termo e resto (xs 
 
 
 >>>>>>> origin/master
+
+
+
+--Sabemos que a soma de uma lista vazia deve ser 0. Escrevemos então como um pattern. 
+-- E também sabemos que a soma de uma lista é o primeiro elemento mais a soma do resto da lista.
+sum' :: (Num a) => [a] -> a  
+sum' [] = 0  
+sum' (x:xs) = x + sum' xs 
+-----------------------------------
+ 
+
+-- "as patterns" Eles são uma mão na roda quando precisamos usar um pattern e continuar com ele acessível, mesmo depois de usado. 
+-- Você cria isso colocando um @ na frente do nome do pattern. Por exemplo, o pattern xs@(x:y:ys). 
+-- Esse "as patterns" irá fazer exatamente a mesma coisa que x:y:ys
+
+capital :: String -> String  
+capital "" = "String vazia, oops!"  
+capital all@(x:xs) = "A primeira letra de " ++ all ++ " é " ++ [x]
+
+
+
+-- função usando guards para decisão
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "Você esta abaixo do peso!"  
+    | bmi <= 25.0 = "Supostamente você esta normal. Pfff, aposto que você é feio!"  
+    | bmi <= 30.0 = "Você esta gordo! Faça uma dieta, gorducho!"  
+    | otherwise   = "Você é uma baleia, meus parabéns!"  
+	
+--------------
+
+bmiTell' :: (RealFloat a) => a -> a -> String  
+bmiTell' weight height  
+    | weight / height ^ 2 <= 18.5 = "Você esta abaixo do peso!"  
+    | weight / height ^ 2 <= 25.0 = "Supostamente você esta normal. Pfff, aposto que você é feio!"  
+    | weight / height ^ 2 <= 30.0 = "Você esta gordo! Faça uma dieta, gorducho!"  
+    | otherwise                 = "Você é uma baleia, meus parabéns!" 
+	
+----
+
+bmiTell'' :: (RealFloat a) => a -> a -> String  
+bmiTell'' weight height  
+    | bmi <= 18.5 = "Você esta abaixo do peso!"  
+    | bmi <= 25.0 = "Supostamente você esta normal. Pfff, aposto que você é feio!"  
+    | bmi <= 30.0 = "Você esta gordo! Faça uma dieta, gorducho!"  
+    | otherwise   = "Você é uma baleia, meus parabéns!"  
+    where bmi = weight / height ^ 2
+
+----
+bmiTell''' :: (RealFloat a) => a -> a -> String  
+bmiTell''' weight height  
+    | bmi <= skinny = "Você esta abaixo do peso!"  
+    | bmi <= normal = "Supostamente você esta normal. Pfff, aposto que você é feio!"  
+    | bmi <= fat    = "Você esta gordo! Faça uma dieta, gorducho!"  
+    | otherwise     = "Você é uma baleia, meus parabéns!"  
+    where bmi = weight / height ^ 2  
+          skinny = 18.5  
+          normal = 25.0  
+          fat = 30.0	
+		  
+		  
+---- usando let e inc
+cylinder :: (RealFloat a) => a -> a -> a  
+cylinder r h = 
+    let sideArea = 2 * pi * r * h  
+        topArea = pi * r ^2  
+    in  sideArea + 2 * topArea 
+	
+	
+------- 
+
+--- RECURSÃO
+-- Recursão é na verdade uma forma de definir funções onde ela própria é usada em sua definição. 
+-- Geralmente definições em matemática são dadas recursivamente. 	
+
+-- fibonacci
+fi(0) = 0
+fi(1) = 1
+fi(n) = fi(n-1) + fi(n-2)
+
+tu :: String -> String 
+tu [] = "Errado"
+tu (d:ds:dsd:dsdx) = "Entrada: " ++[d] ++ " " ++ [ds] ++ "" ++ [dsd]
+
+maximum' :: (Ord a) => [a] -> a  
+maximum' [] = error "maximum of empty list"  
+maximum' [x] = x  
+maximum' (x:xs) = max x (maximum' xs) 
